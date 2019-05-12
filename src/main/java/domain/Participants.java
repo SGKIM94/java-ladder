600eddb52d47359f1d1c4b9f93240a94a88f7a9f
SGKIM94 <tkdrn8578@naver.com>
2019-05-12 14:54:40 +0900

    [MODIFY] Participants 일급 콜렉션 추가  * Ladder 일급 콜렉션 객체 만든 것과 같은 이유로 Paticipant 객체를 생성  * 참가자 들을 출력할 수 있도록 printFormat도 추가함

---
+++ src/main/java/domain/Participants.java
@@ -0,0 +1,48 @@
+package domain;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Participants {
+    private List<String> participants;
+
+    public Participants(String names) {
+        this.participants = setParticipant(names);
+    }
+
+    public ArrayList<String> setParticipant(String input) {
+        String [] names = separatorNames(input);
+        return new ArrayList<>(Arrays.asList(names));
+    }
+
+    public String[] separatorNames(String input) {
+        checkNull(input);
+        return input.split(",");
+    }
+
+    public void checkNull(String input) {
+        if (StringUtils.isEmpty(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Participants add(String name) {
+        this.participants.add(name);
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String participant : participants) {
+            stringBuilder.append(participant + "  ");
+        }
+
+        return String.valueOf(stringBuilder);
+    }
+}
---
+++ src/main/java/view/participantsFormat.java
@@ -0,0 +1,9 @@
+package view;
+
+import domain.Participants;
+
+public class participantsFormat {
+    public static void printParticipants(Participants participants) {
+        System.out.println(participants.toString());
+    }
+}
