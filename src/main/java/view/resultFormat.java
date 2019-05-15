a24e75dde3d1eecd128ce8c92d2f6bb6668b9459
SGKIM94 <tkdrn8578@naver.com>
2019-05-15 23:10:27 +0900

    [ADD] 결과를 출력해주는 view 클래스 추가

---
+++ src/main/java/view/resultFormat.java
@@ -0,0 +1,9 @@
+package view;
+
+import domain.Results;
+
+public class resultFormat {
+    public static void printResults(Results results) {
+        System.out.println(results.toString());
+    }
+}
