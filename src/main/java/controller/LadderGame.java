be47b46a1aac436a92b45d1118da0b581439d3a3
SGKIM94 <tkdrn8578@naver.com>
2019-05-15 23:10:11 +0900

    [ADD] 결과를 리턴해줄 수 있도록 main 메서드에 result 출력 객체 생성 및 추가

--- src/main/java/controller/LadderGame.java
+++ src/main/java/controller/LadderGame.java
@@ -1,17 +1,21 @@
 package controller;
 
 import domain.Participants;
+import domain.Results;
 
 import static view.LadderFormat.printLadder;
 import static view.participantsFormat.printParticipants;
+import static view.resultFormat.printResults;
 
 public class LadderGame {
 
-    public static void main(String names, int height) {
+    public static void main(String names, int height, String result) {
         domain.LadderGame ladderGame = new domain.LadderGame(height, names);
         Participants participants = new Participants(names);
+        Results results = new Results(result);
 
         printParticipants(participants);
         printLadder(ladderGame);
+        printResults(results);
     }
 }
--- src/test/java/controller/LadderGameTest.java
+++ src/test/java/controller/LadderGameTest.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 public class LadderGameTest {
     static final String NAMES = "kim,choi,pack,lee";
     static final int HEIGHT = 4;
+    static final String RESULTS = "꽝,500,꽝,1000";
 
     @Test
     public void 사다리_게임_테스트() {
-        LadderGame.main(NAMES, HEIGHT);
+        LadderGame.main(NAMES, HEIGHT, RESULTS);
     }
 }
