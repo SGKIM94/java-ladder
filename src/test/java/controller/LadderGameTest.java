23540792dd5326b09f4acd61bde0effb03939bb6
SGKIM94 <tkdrn8578@naver.com>
2019-05-22 23:09:11 +0900

    [ADD] main 메서드를 테스트하는 테스트코드 추가

--- src/test/java/controller/LadderGameTest.java
+++ src/test/java/controller/LadderGameTest.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 public class LadderGameTest {
     static final String NAMES = "kim,choi,pack,lee";
     static final int HEIGHT = 4;
-    static final String RESULTS = "꽝,500,꽝,1000";
+    static final String RESULTS = "1,2,3,4";
+    static final String RESULT_PARTICIPANT = "choi";
+    public static final String ALL = "all";
 
     @Test
     public void 사다리_게임_테스트() {
-        LadderGame.main(NAMES, HEIGHT, RESULTS);
+        LadderGame.main(NAMES, HEIGHT, RESULTS, RESULT_PARTICIPANT);
+    }
+
+    @Test
+    public void 사다리_게임_전체_출력_테스트() {
+        LadderGame.main(NAMES, HEIGHT, RESULTS, ALL);
     }
 }
