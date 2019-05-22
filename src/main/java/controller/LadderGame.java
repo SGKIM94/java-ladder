85c16aa4f494764b5119facfd16181fcabe060cf
SGKIM94 <tkdrn8578@naver.com>
2019-05-22 23:07:25 +0900

    [MODIFY] 최종 매칭된 결과 값을 리턴하는 로직 main에 추가

--- src/main/java/controller/LadderGame.java
+++ src/main/java/controller/LadderGame.java
@@ -1,21 +1,43 @@
 package controller;
 
-import domain.Participants;
-import domain.Results;
+import domain.*;
 
+import static view.InputResultFormat.printInputResult;
 import static view.LadderFormat.printLadder;
-import static view.participantsFormat.printParticipants;
-import static view.resultFormat.printResults;
+import static view.ParticipantsFormat.printParticipants;
+import static view.ResultFormat.printResults;
 
 public class LadderGame {
+    private static final String SHOW_ALL_RESULTS = "all";
 
-    public static void main(String names, int height, String result) {
+    public static void main(String names, int height, String result, String wantToShowResult) {
         domain.LadderGame ladderGame = new domain.LadderGame(height, names);
         Participants participants = new Participants(names);
         Results results = new Results(result);
+        int horizonSize = participants.getSize();
+        LadderSize ladderSize = new LadderSize(height, horizonSize);
 
         printParticipants(participants);
         printLadder(ladderGame);
         printResults(results);
+
+        System.out.println("결과를 보고싶은 사람은?");
+        System.out.println(wantToShowResult);
+
+        if (SHOW_ALL_RESULTS.equals(wantToShowResult)) {
+            showAllResult(ladderGame, participants, results, ladderSize);
+            return;
+        }
+
+        int resultIndex = new LadderResults(participants.getParticipantIndex(wantToShowResult), ladderSize)
+                            .makeResult(ladderGame);
+        printInputResult(results, resultIndex, wantToShowResult);
+    }
+
+    private static void showAllResult(domain.LadderGame ladderGame, Participants participants, Results results, LadderSize ladderSize) {
+        int horizonSize = ladderSize.getHorizon();
+        for (int i = 0; i < horizonSize; i++) {
+            printInputResult(results, new LadderResults(i, ladderSize).makeResult(ladderGame), participants.get(i));
+        }
     }
 }
