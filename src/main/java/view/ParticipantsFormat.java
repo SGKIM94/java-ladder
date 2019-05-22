d99af7b0cdcec2cbe1b21c52b6a66f825ebe10d0
SGKIM94 <tkdrn8578@naver.com>
2019-05-22 23:13:42 +0900

    [ADD] 참가자 형태를 출력하는 메서드 추가

--- src/main/java/view/ParticipantsFormat.java
+++ src/main/java/view/ParticipantsFormat.java
@@ -2,7 +2,7 @@ package view;
 
 import domain.Participants;
 
-public class participantsFormat {
+public class ParticipantsFormat {
     public static void printParticipants(Participants participants) {
         System.out.println(participants.toString());
     }
